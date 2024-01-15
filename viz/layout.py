7a5dc63117f36236ca308aa0ed7bca9e27a8ee1c
Luunynliny <thomasmattone.perso@gmail.com>
2024-01-15 16:34:36 +0100

    switch between maps

--- viz/layout.py
+++ viz/layout.py
@@ -1,11 +1,30 @@
+import dash_daq as daq
 from components import train_stop_density_map, train_stop_scatter_map
-from dash import Dash, dcc, html
+from dash import Dash, Input, Output, callback, dcc, html
 
 viz = Dash(__name__)
 
 viz.layout = html.Div(
     [
-        dcc.Graph(figure=train_stop_scatter_map),
-        dcc.Graph(figure=train_stop_density_map),
+        daq.ToggleSwitch(
+            id="switch_map",
+            label=["Train stop locations", "Train stop density"],
+            value=False,
+        ),
+        dcc.Graph(
+            id="map", figure=train_stop_scatter_map, style={"height": "90vh"}
+        ),
     ]
 )
+
+
+@callback(
+    Output("map", "figure"),
+    Input("switch_map", "value"),
+)
+def switch_map(is_density_selected):
+    return (
+        train_stop_density_map
+        if is_density_selected
+        else train_stop_scatter_map
+    )

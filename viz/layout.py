adf82cf2348e4cb5763036514b326e879abe9d38
Luunynliny <thomasmattone.perso@gmail.com>
2024-01-15 16:53:38 +0100

    synchronize maps

--- viz/layout.py
+++ viz/layout.py
@@ -20,11 +20,22 @@ viz.layout = html.Div(
 
 @callback(
     Output("map", "figure"),
-    Input("switch_map", "value"),
+    [Input("switch_map", "value"), Input("map", "relayoutData")],
 )
-def switch_map(is_density_selected):
-    return (
+def switch_map(is_density_selected, relayout_data):
+    fig = (
         train_stop_density_map
         if is_density_selected
         else train_stop_scatter_map
     )
+
+    if relayout_data:
+        # Store the zoom and pan information from the previous map
+        prev_zoom = relayout_data.get("mapbox.zoom", 10)
+        prev_center = relayout_data.get("mapbox.center", {"lat": 0, "lon": 0})
+
+        # Apply the stored zoom and pan to the new map
+        fig["layout"]["mapbox"]["zoom"] = prev_zoom
+        fig["layout"]["mapbox"]["center"] = prev_center
+
+    return fig
